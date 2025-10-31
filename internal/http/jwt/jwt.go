e9ee885b595982ae138b9a6c3ce8825a425669a4
Dani Ihza Farrosi <dani.videda@gmail.com>
2025-10-31 09:29:42 +0700

    refresh and access token secret from env

--- internal/http/jwt/jwt.go
+++ internal/http/jwt/jwt.go
@@ -3,16 +3,27 @@ package jwt
 import (
 	"time"
 
+	"github.com/danivideda/satu-apotek-be/internal/env"
 	"github.com/golang-jwt/jwt/v5"
 )
+
+var refreshTokenKey = env.GetString("JWT_REFRESH_SECRET", "refresh-token-secret")
+var accessTokenKey = env.GetString("JWT_ACCESS_SECRET", "access-token-secret")
 
 type myClaim struct {
 	ID string `json:"id"`
 	jwt.RegisteredClaims
 }
 
-const refreshTokenKey string = "refresh-token-secret-key"
-const accessTokenKey string = "refresh-token-secret-key"
+func NewRefreshToken(id string) (string, error) {
+	exp := time.Now().Add(24 * time.Hour)
+	return generate(id, exp, refreshTokenKey)
+}
+
+func NewAccessToken(id string) (string, error) {
+	exp := time.Now().Add(1 * time.Minute)
+	return generate(id, exp, accessTokenKey)
+}
 
 func generate(id string, exp time.Time, signKey string) (string, error) {
 	claims := myClaim{
@@ -26,14 +37,4 @@ func generate(id string, exp time.Time, signKey string) (string, error) {
 
 	t := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return t.SignedString(signKey)
-}
-
-func NewRefreshToken(id string) (string, error) {
-	exp := time.Now().Add(24 * time.Hour)
-	return generate(id, exp, refreshTokenKey)
-}
-
-func NewAccessToken(id string) (string, error) {
-	exp := time.Now().Add(1 * time.Minute)
-	return generate(id, exp, accessTokenKey)
 }
