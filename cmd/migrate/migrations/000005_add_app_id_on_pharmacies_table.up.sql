53f31bbcaebdd80f83a2ea74f6f520156989062b
Dani Ihza Farrosi <dani.videda@gmail.com>
2026-03-27 06:44:30 +0700

    fix: unique for app_id column

--- cmd/migrate/migrations/000005_add_app_id_on_pharmacies_table.up.sql
+++ cmd/migrate/migrations/000005_add_app_id_on_pharmacies_table.up.sql
@@ -1,4 +1,4 @@
 ALTER TABLE IF EXISTS pharmacies
-ADD COLUMN IF NOT EXISTS app_id varchar(15) NOT NULL DEFAULT '0';
+ADD COLUMN IF NOT EXISTS app_id varchar(15) UNIQUE NOT NULL DEFAULT '0';
 
 CREATE INDEX pharmacies_app_id_idx ON pharmacies (app_id);
